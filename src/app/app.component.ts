91c42fb7419ffb67a3e4a13e8b689f0039e802fd
carlo.bonamico@gmail.com <carlo.bonamico@gmail.com>
2018-11-28 08:11:41 +0100

    fix

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { BookClientService } from './book-client.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'angular-professional-workshop';
+
+  books = [];
+
+  constructor(private bookClient: BookClientService) {
+
+  }
+
+  ngOnInit() {
+    this.bookClient.getBooks('topic').subscribe( (bookData: any) => {
+      this.books = bookData.hits.hits;
+
+    });
+  }
 }
